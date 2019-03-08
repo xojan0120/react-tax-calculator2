41e7024db0f2c17a2f9bb49fe6e88b3838123c1b
xojan0120 <xojan0120@gmail.com>
2019-03-08 17:50:29 +0900

    [Add] buildpack

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'build')));
-const port = process.env.PORT || 3000;
-const server = app.listen(port, function () {
-  console.log('server start');
-});
